6c4d71955a37051d86fcc8575881c7d3986af961
zymfe <1047832475@qq.com>
2019-09-28 18:33:42 +0800

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -6,4 +6,4 @@ tar -czvf weapp.tar.gz .
 mkdir -p /work/api/weapp
 rm -rf /work/api/weapp/*
 cp weapp.tar.gz /work/api/weapp
-cd /work/api/weapp && tar -xvf weapp.tar.gz && npm --registry=https://registry.npm.taobao.org install && pm2 stop pm2.json && pm2 start weapp
+cd /work/api/weapp && tar -xvf weapp.tar.gz && npm --registry=https://registry.npm.taobao.org install && pm2 stop pm2.json && pm2 start pm2.json
