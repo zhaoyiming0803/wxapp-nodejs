6fc647d3ffdb245e6932b37df6355437c4691908
zymfe <1047832475@qq.com>
2019-09-29 16:14:00 +0800

    get openGid

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const Router = require('koa-router');
 const router = new Router();
 const bodyParser = require('koa-bodyparser');
 const WxCrypt = require('./libs/WxCrypt');
+const WXBizDataCrypt = require('./libs/WXBizDataCrypt');
 const { weapp } = require('./config');
+const request = require('./helper/request');
 
 app.use(bodyParser());
 
@@ -30,6 +32,21 @@ router.post('/weapp/', async (ctx, next) => {
   const { ToUserName, Encrypt } = ctx.request.body;
 
   console.log(JSON.parse(wxCrypt.decrypt(Encrypt)));
+});
+
+router.post('/weapp/shareTicket', async (ctx, next) => {
+  const { encryptedData, iv, code } = ctx.request.body;
+  const result = await request({
+    url: 'https://api.weixin.qq.com/sns/jscode2session' +
+      `?appid=${weapp.appID}` +
+      `&secret=${weapp.appSecret}` +
+      `&js_code=${code}` +
+      '&grant_type=authorization_code'
+  });
+  console.log(result)
+  const pc = new WXBizDataCrypt(weapp.appID, result.session_key);
+  const data = pc.decryptData(encryptedData , iv);
+  console.log('解密后 data: ', data);
 });
 
 app.use(router.routes(), router.allowedMethods());
